fb42aa5fa96454561cfe413ad5485c006737afe8
AJ-19-Git <cordero.alejandro09@gmail.com>
2026-04-16 15:53:03 -0400

    Refactor item class and subclasses
    Refactor item class and its subclasses to include name attribute and adjust constructors.

--- Items.java
+++ Items.java
@@ -1,11 +1,28 @@
 import java.util.*
 
-public class item(){
+public abstract item(){
   private int ID;
-  private int type;
   private int quantity;
+  private String name;
+}
+public item(int ID, String name, int quantity){
+  this.ID = ID;
+  this.quantity = quantity;
+  this.name = name;
+  
 }
 
-public class weapon extends item(int ID, int quantity) {
+public class weapon extends item(int ID, String name, int quantity) {
+  //int atkPlus;
+  int atk;
+  public void Attack(Entity user){
+    user.attack
+  }
+}
+public class armor extends item(int ID,String name, int quantity) {
+ //int defPlus;
+ //int acPlus;
+}
+public class consumable extends item(int ID,String name, int quantity) {
   
 }
