531f6bad644cd0f73a5ebc87ef8e11b717eca7a6
anthonylevrai <pergolizzianthony@gmail.com>
2026-04-24 23:34:52 -0400

    Implement CombatResolver system

--- core/src/main/java/com/teamfive/dauntlessdungeoneer/combat/results/CombatResult.java
+++ core/src/main/java/com/teamfive/dauntlessdungeoneer/combat/results/CombatResult.java
@@ -1,5 +1,19 @@
 package com.teamfive.dauntlessdungeoneer.combat.results;
 
+import com.teamfive.dauntlessdungeoneer.ecs.Entity;
+
 public class CombatResult {
 
+    public final Entity attacker;
+    public final Entity defender;
+
+    public final boolean didHit;
+    public final int damageDealt;
+
+    public CombatResult(Entity attacker, Entity target,boolean didHit, int damageDealt) {
+        this.attacker = attacker;
+        this.defender = target;
+        this.didHit = didHit;
+        this.damageDealt = damageDealt;
+    }
 }
--- core/src/main/java/com/teamfive/dauntlessdungeoneer/combat/systems/CombatResolver.java
+++ core/src/main/java/com/teamfive/dauntlessdungeoneer/combat/systems/CombatResolver.java
@@ -1,5 +1,55 @@
 package com.teamfive.dauntlessdungeoneer.combat.systems;
 
+import com.teamfive.dauntlessdungeoneer.combat.results.CombatResult;
+import com.teamfive.dauntlessdungeoneer.combat.results.DamageResult;
+import com.teamfive.dauntlessdungeoneer.combat.results.HitResult;
+import com.teamfive.dauntlessdungeoneer.ecs.Entity;
+
 public class CombatResolver {
 
+    private final TargetingSystem targetingSystem;
+    private final AccuracySystem accuracySystem;
+    private final DamageSystem damageSystem;
+    private final HealthSystem healthSystem;
+
+    public CombatResolver(
+        TargetingSystem targetingSystem,
+        AccuracySystem accuracySystem,
+        DamageSystem damageSystem,
+        HealthSystem healthSystem
+    ) {
+        this.targetingSystem = targetingSystem;
+        this.accuracySystem = accuracySystem;
+        this.damageSystem = damageSystem;
+        this.healthSystem = healthSystem;
+    }
+
+    public CombatResult resolveAttack(Entity attacker, Entity target) {
+
+        // Validate target
+        if (!targetingSystem.isValidEnemyTarget(attacker, target)) {
+            return new CombatResult(attacker, target, false, 0);
+        }
+
+        // Accuracy check
+        HitResult hitResult = accuracySystem.determineHit(attacker, target);
+
+        if (!hitResult.didHit) {
+            return new CombatResult(attacker, target, false, 0);
+        }
+
+        // Damage calculation
+        DamageResult damageResult = damageSystem.calculateDamage(attacker, target);
+
+        // Apply damage
+        healthSystem.applyDamage(target, damageResult.finalDamage);
+
+        // Return result
+        return new CombatResult(
+            attacker,
+            target,
+            true,
+            damageResult.finalDamage
+        );
+    }
 }
