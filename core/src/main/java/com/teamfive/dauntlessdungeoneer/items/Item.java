c6f55b2f940491653427f1b5eb4e8e44eb2c3081
AJ-19 <cordero.alejandro09@gmail.com>
2026-04-28 16:04:18 -0400

    Added the set of items, 4 weapons, 3 armors, and 2 potions!

--- core/src/main/java/com/teamfive/dauntlessdungeoneer/items/Item.java
+++ core/src/main/java/com/teamfive/dauntlessdungeoneer/items/Item.java
@@ -31,4 +31,19 @@ public class Item {
     public int getType() { return type; }
     public int getStatBonus() {return statBonus;}
     public int getQuantity() { return quantity; }
+
+    Item lightSword = new Item(1,"Short Sword",1,10,1);
+    Item heavySword = new Item(2,"Broad Sword",1,50,1);
+    Item orb = new Item(3,"Magic Orb", 1,20,1);
+    Item staff = new Item(4,"Staff",1,30,1);
+    Item shield = new Item(5,"Shield",2,10,1);
+    Item lightArmor = new Item(6,"Light Armor",2,30,1);
+    Item heavyArmor = new Item(7,"Heavy Armor",2,50,1);
+    Item recoveryPotion = new Item(8,"Recovery Potion",3,50,2);
+    Item damagePotion = new Item(9,"Harmful Potion",3,20,2);
+
+
+
+
+
 }
